abecd64902522dd29dc9d8f3057165e9db634193
woowenjun99 <woowenjun99@gmail.com>
2024-01-06 00:41:51 +0800

    Write logic to save a module

---
+++ functions/src/services/usermodules/saveModule.spec.ts
@@ -0,0 +1,86 @@
+import { createUser, deleteUser } from "../../db/sqlc/user_sql";
+import {
+    deleteModule,
+    saveModule,
+    findModuleById,
+} from "../../db/sqlc/usermodules_sql";
+import { v4 as uuid } from "uuid";
+import { pool } from "../../util/setup";
+import { SaveModuleService } from "./saveModule";
+
+describe("EditUserService", () => {
+    it("should throw if the module has already been saved", async () => {
+        const id = uuid();
+        const email = `${uuid()}@gmail.com`;
+
+        await createUser(pool, {
+            email,
+            fullName: "Woo Wen Jun",
+            id,
+            major: "Computer Engineer",
+            semester: 0,
+        });
+
+        await saveModule(pool, {
+            moduleCode: "CS2040C",
+            userId: id,
+            semesterTaken: 7,
+        });
+
+        const service = new SaveModuleService(
+            {
+                moduleCode: "CS2040C",
+                semesterTaken: 8,
+            },
+            id
+        );
+
+        await expect(service.execute()).rejects.toThrowError(
+            "Module has already been saved"
+        );
+
+        await deleteModule(pool, { userId: id, moduleCode: "CS2040C" });
+
+        await deleteUser(pool, { id });
+    });
+
+    it("should save the module correctly", async () => {
+        const id = uuid();
+        const email = `${uuid()}@gmail.com`;
+
+        await createUser(pool, {
+            email,
+            fullName: "Woo Wen Jun",
+            id,
+            major: "Computer Engineer",
+            semester: 0,
+        });
+
+        const service = new SaveModuleService(
+            {
+                moduleCode: "CS2040C",
+                semesterTaken: 8,
+            },
+            id
+        );
+
+        await service.execute();
+
+        const module = await findModuleById(pool, {
+            moduleCode: "CS2040C",
+            userId: id,
+        });
+
+        expect(module).toBeDefined()
+        expect(module?.moduleCode).toStrictEqual("CS2040C")
+        expect(module?.grades).toBeNull()
+        expect(module?.userId).toStrictEqual(id)
+
+        await deleteModule(pool, {
+            moduleCode: "CS2040C",
+            userId: id,
+        })
+
+        await deleteUser(pool, { id })
+    });
+});
---
+++ functions/src/services/usermodules/saveModule.ts
@@ -0,0 +1,46 @@
+import {
+  type FindModuleByIdArgs,
+  type SaveModuleArgs,
+  findModuleById,
+  saveModule,
+} from "../../db/sqlc/usermodules_sql";
+import { pool } from "../../db/pool";
+import { z } from "zod";
+
+type SaveModuleServiceRequest = Readonly<{
+  moduleCode: string
+  semesterTaken: number
+}>
+
+export class SaveModuleService {
+  constructor(private payload: SaveModuleServiceRequest, private uid: string) {}
+
+  private async validate(): Promise<void> {
+    // Validate the input schema.
+    this.payload = z.object({
+      moduleCode: z.string(),
+      semesterTaken: z.number().int().positive(),
+    }).parse(this.payload);
+
+    // Check if the user has saved the module before.
+    const module = await findModuleById(pool, {
+      moduleCode: this.payload.moduleCode,
+      userId: this.uid,
+    } as FindModuleByIdArgs);
+
+    if (module) throw Error("Module has already been saved");
+
+    return Promise.resolve();
+  }
+
+  public async execute() {
+    await this.validate();
+
+    // Save the module as per the request
+    await saveModule(pool, {
+      moduleCode: this.payload.moduleCode,
+      semesterTaken: this.payload.semesterTaken,
+      userId: this.uid,
+    } as SaveModuleArgs);
+  }
+}
