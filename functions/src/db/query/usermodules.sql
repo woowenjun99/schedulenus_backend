88d5ecec2183889cfd357a075fe2bca2cdc03a79
woowenjun99 <woowenjun99@gmail.com>
2024-01-06 00:41:51 +0800

    Write logic to save a module

--- functions/src/db/query/usermodules.sql
+++ functions/src/db/query/usermodules.sql
@@ -2,3 +2,13 @@
 INSERT INTO usermodules
 (module_code, semester_taken, user_id)
 VALUES ($1, $2, $3);
+
+-- name: FindModuleById :one
+SELECT *
+FROM usermodules
+WHERE user_id = $1 AND module_code = $2
+LIMIT 1 OFFSET 0;
+
+-- name: DeleteModule :exec
+DELETE FROM usermodules
+WHERE user_id = $1 AND module_code = $2;
--- functions/src/db/sqlc/usermodules_sql.ts
+++ functions/src/db/sqlc/usermodules_sql.ts
@@ -23,3 +23,56 @@ export async function saveModule(client: Client, args: SaveModuleArgs): Promise<
     });
 }
 
+export const findModuleByIdQuery = `-- name: FindModuleById :one
+SELECT grades, module_code, semester_taken, user_id
+FROM usermodules
+WHERE user_id = $1 AND module_code = $2
+LIMIT 1 OFFSET 0`;
+
+export interface FindModuleByIdArgs {
+    userId: string;
+    moduleCode: string;
+}
+
+export interface FindModuleByIdRow {
+    grades: number | null;
+    moduleCode: string;
+    semesterTaken: number;
+    userId: string;
+}
+
+export async function findModuleById(client: Client, args: FindModuleByIdArgs): Promise<FindModuleByIdRow | null> {
+    const result = await client.query({
+        text: findModuleByIdQuery,
+        values: [args.userId, args.moduleCode],
+        rowMode: "array"
+    });
+    if (result.rows.length !== 1) {
+        return null;
+    }
+    const row = result.rows[0];
+    return {
+        grades: row[0],
+        moduleCode: row[1],
+        semesterTaken: row[2],
+        userId: row[3]
+    };
+}
+
+export const deleteModuleQuery = `-- name: DeleteModule :exec
+DELETE FROM usermodules
+WHERE user_id = $1 AND module_code = $2`;
+
+export interface DeleteModuleArgs {
+    userId: string;
+    moduleCode: string;
+}
+
+export async function deleteModule(client: Client, args: DeleteModuleArgs): Promise<void> {
+    await client.query({
+        text: deleteModuleQuery,
+        values: [args.userId, args.moduleCode],
+        rowMode: "array"
+    });
+}
+
